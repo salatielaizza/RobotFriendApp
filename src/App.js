91f9e91131c259569b1f363547b69433f6be46e3
Salatiel Aizza <salatiel.aizza@gmail.com>
2019-06-09 17:24:08 +0100

    fix trouble with imports and add Scroll.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,56 @@
-import React , {Component} from 'react';
-import  robots1 from './robots';
-import  CardList from './CardList';
+import React, { Component } from 'react';
+import CardList from './CardList';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll'
+//import App from './App'
+
 
 class App extends Component {
-    constructor(){
+    constructor() {
         super()
         this.state = {
-            robots: robots1,
+            robots: [],
             searchfield: ''
         }
     }
 
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value });
+      componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => {
+                return response.json();
+            })
+            .then(users => {
+                this.setState({ robots: users })
+            });
     }
-    render(){
-        const filteredRobots = this.state.robots.filter(robots => {
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+
+    onSearchChange = (event) => {
+        this.setState({
+            searchfield:
+                event.target.value
+        });
+    }
+
+
+
+    render() {
+        // const {robots, searchfield} = this.state; I can erase next 3 "this.state"
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         })
-        return (
-            <div className ='tc'>
-                <h1>Robot Friend</h1>
-                <SearchBox searchChange={this.onSearchChange}/> 
-                <CardList robots = {filteredRobots} /> 
-            </div>
-        );
+        if (!this.state.robots.length) {   //robots.lenght is igual zero
+            return <h1>Loading</h1>
+        } else {
+            return (
+                <div className='tc f4'>
+                    <h1>RoboFriend</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <Scroll>
+                        <CardList robots={filteredRobots} />
+                    </Scroll>
+                </div>
+            );
+        }
     }
 }
 

891e7ae2084070b64652cd1b89ce35fc522246d0
Salatiel Aizza <salatiel.aizza@gmail.com>
2019-06-12 10:00:15 +0100

    added folders containers and conponents

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Scroll = (props) => {
     return (
-        <div style={{overflowY: 'scroll', border: '5px solid black', height: '500px'}}>
+        <div style={{overflowY: 'scroll', border: '5px solid black', height: '800px'}}>
             {props.children}
         </div>
     )
